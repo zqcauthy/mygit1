7c4ce8df8a341c1784375b437a678d88ad5457b2
zhangqiangq <azz1025@163.com>
2021-12-03 10:43:30 +0800

    修改test01.java

--- src/main/java/com/bjpowernode/Test01.java
+++ src/main/java/com/bjpowernode/Test01.java
@@ -12,5 +12,8 @@ package com.bjpowernode;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01...");
+        System.out.println("test02...");
+        System.out.println("test03...");
+        System.out.println("test04...");
     }
 }
