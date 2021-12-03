75812e1834e0dcb3cee0e89e59d761a18ec9f6ba
zhangqiangq <azz1025@163.com>
2021-12-03 11:03:02 +0800

    dev分支的test02提交到本地卡

---
+++ src/main/java/com/bjpowernode/test02.java
@@ -0,0 +1,16 @@
+package com.bjpowernode;
+
+/**
+ * ClassName: test02
+ * Package: com.bjpowernode
+ * Description:
+ *
+ * @Author: azz1025@163.com
+ * @Date: 2021/12/3 11:01
+ * @Version: 1.0
+ */
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("test02...");
+    }
+}
