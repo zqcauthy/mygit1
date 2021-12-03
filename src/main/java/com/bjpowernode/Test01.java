1167c171f11f5aee5d3907657a329543f0734402
zhangqiangq <azz1025@163.com>
2021-12-03 11:40:54 +0800

    dev的test01更新操作

--- src/main/java/com/bjpowernode/Test01.java
+++ src/main/java/com/bjpowernode/Test01.java
@@ -15,5 +15,7 @@ public class Test01 {
         System.out.println("test02...");
         System.out.println("test03...");
         System.out.println("test04...");
+        System.out.println("dev-->test04...");
+
     }
 }
