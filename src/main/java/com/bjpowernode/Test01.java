87e5a0f602639d3c5d626e5cb5979437857388f6
zhangqiangq <azz1025@163.com>
2021-12-03 10:33:44 +0800

    添加test01.java

---
+++ src/main/java/com/bjpowernode/Test01.java
@@ -0,0 +1,16 @@
+package com.bjpowernode;
+
+/**
+ * ClassName: Test01
+ * Package: com.bjpowernode
+ * Description:
+ *
+ * @Author: azz1025@163.com
+ * @Date: 2021/12/3 10:32
+ * @Version: 1.0
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("test01...");
+    }
+}
